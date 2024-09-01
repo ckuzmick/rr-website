b6ffd55d1834fd18b7b9c4da8a1e51bc8339f335
ckuzmick <conrad.kuzmick@icloud.com>
2024-09-01 13:34:26 -0400

    fix ascii

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,23 @@
 import "@/styles/home.css";
-import Ascii from "@/components/ascii";
+import Image from "next/image";
 
-export default function Page() {
-    return (
-        <div id="site-home">
-            <h1 id="first-edition-subtitle">CRLS Free Thinkers Journal</h1>
-            <div id="divider" />
-            <h1 id="first-edition-title">The First Edition</h1>
-            <Ascii id="ascii-art" file="/ascii-art/1-cover.txt" />
-            <div id="articles-list">
-                <p>An Open Letter To The Register Forum</p>
-                <p>Brawl Stars is Destroying Our World</p>
-                <p>Getting Back on Track After Detracking</p>
-                <p>An Exclusive Interview With ...</p>
-                <p>+ What is The Free Thinkers Journal?</p>
-            </div>
-        </div>
-    )
+export default function Page({ asciiText }) {
+  return (
+    <div id="site-home">
+      <h1 id="first-edition-subtitle">CRLS Free Thinkers Journal</h1>
+      <div id="divider" />
+      <h1 id="first-edition-title">The First Edition</h1>
+      <Image src="/ascii-art/cover-1.svg" alt="First Edition" width={0}
+  height={0}
+  sizes="100vw"
+  style={{ width: '100%', height: 'auto' }} id="ascii-art" />
+      <div id="articles-list">
+        <p>An Open Letter To The Register Forum</p>
+        <p>Brawl Stars is Destroying Our World</p>
+        <p>Getting Back on Track After Detracking</p>
+        <p>An Exclusive Interview With ...</p>
+        <p>+ What is The Free Thinkers Journal?</p>
+      </div>
+    </div>
+  );
 }
