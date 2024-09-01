a24a8ed8fe0f51f0d46b0e39919a822a946d841d
ckuzmick <conrad.kuzmick@icloud.com>
2024-08-31 20:50:06 -0400

    added article placeholders

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,13 @@ export default function Page() {
             <div id="divider" />
             <h1 id="first-edition-title">The First Edition</h1>
             <Ascii id="ascii-art" file="/ascii-art/1-cover.txt" />
+            <div id="articles-list">
+                <p>An Open Letter To The Register Forum</p>
+                <p>Brawl Stars is Destroying Our World</p>
+                <p>Getting Back on Track After Detracking</p>
+                <p>An Exclusive Interview With ...</p>
+                <p>+ What is The Free Thinkers Journal?</p>
+            </div>
         </div>
     )
 }
