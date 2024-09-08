ddea2f14c17ba1dbce2cfadafd637135d97d4dd9
ckuzmick <conrad.kuzmick@icloud.com>
2024-09-07 20:33:18 -0400

    test

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function Page({ asciiText }) {
         <h1 id="first-edition-subtitle">Rindge Radical</h1>
         <div id="divider" />
         <h1 id="first-edition-title">The First Edition</h1>
-        <Image src="/ascii-art/cover-1.svg" alt="First Edition" width={0}
+        <Image src="/ascii-art/cover-1.svg" alt="Cover Photo: CRLS Exterior" width={0}
           height={0}
           sizes="100vw"
           style={{ width: '100%', height: 'auto' }} id="ascii-art" />
