8e2ddc864db1a5fecb7b13a2f307248c3c32ee3e
ckuzmick <conrad.kuzmick@icloud.com>
2024-10-27 19:01:20 -0400

    stuff

--- src/app/writing/[slug]/page.js
+++ src/app/writing/[slug]/page.js
@@ -35,7 +35,7 @@ export default async function ArticlePage({ params }) {
             />
             <div className="markdown-content">
                 {article.content.split("\n").map((paragraph, index) => (
-                    <ReactMarkdown components={{ a: LinkRenderer}}>{paragraph}</ReactMarkdown>
+                    <ReactMarkdown key={index} components={{ a: LinkRenderer}}>{paragraph}</ReactMarkdown>
                 ))}
             </div>
         </article>
